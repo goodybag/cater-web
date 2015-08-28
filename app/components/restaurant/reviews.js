b2a4c1492af1a5a1d25e52b64ab51df364339fcb
m-amaya <marissa.amaya.311@gmail.com>
2015-08-28 08:57:25 -0500

    Created <RestaurantReviewsComponent>

---
+++ app/components/restaurant/reviews.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+export class RestaurantReviewsComponent extends Component {
+    static propTypes = {
+
+    }
+
+    render() {
+        return (
+            <div className="gb-restaurant-reviews">
+                {/* TODO */}
+            </div>
+        )
+    }
+}
