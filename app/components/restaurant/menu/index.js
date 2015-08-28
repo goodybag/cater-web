d9a343190f651dfd5ca81547145d466280770c73
m-amaya <marissa.amaya.311@gmail.com>
2015-08-28 08:56:00 -0500

    Added <RestaurantMenuComponent>

---
+++ app/components/restaurant/menu/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+export class RestaurantMenuComponent extends Component {
+    static propTypes = {
+
+    }
+
+    render() {
+        return (
+            <div className="gb-restaurant-menu">
+                {/* TODO */}
+            </div>
+        );
+    }
+}
