00a0325fe010107330f18dd1f565c2ae89dc0054
m-amaya <marissa.amaya.311@gmail.com>
2015-10-04 17:14:06 -0500

    Added color codes to time remaining panel

--- app/components/order-pane/timeleft.js
+++ app/components/order-pane/timeleft.js
@@ -12,18 +12,56 @@ export class OrderPaneTimeLeftComponent extends Component {
         const currentTime = moment().tz(timezone);
         const scheduledTime = moment.tz(datetime, timezone);
         const daysRemaining = scheduledTime.diff(currentTime, 'days');
+        const hoursRemaining = scheduledTime.diff(currentTime, 'hours');
+
+        if(!daysRemaining) {
+            console.log("calculate hours: " + hoursRemaining);
+        }
 
         return (
             <div className="gb-order-pane-timeleft">
                 <i className="icon-timer"></i>
                 TIME LEFT TO PLACE ORDER:
                 <span className="gb-order-pane-timeleft-time">
-                    {
-                        daysRemaining===1 ? "1 DAY" :
-                        ( daysRemaining + " DAYS" )
-                    }
+                    {renderTimeRemaining()}
                 </span>
             </div>
         );
+
+        function renderTimeRemaining() {
+            if(!daysRemaining) {
+                if(!hoursRemaining) {
+                    return (
+                        <span className="gb-no-time-remaining">
+                            { hoursRemaining + " HRS" }
+                        </span>
+                    );
+                } else if (hoursRemaining===1) {
+                    return (
+                        <span className="gb-no-time-remaining">
+                            { hoursRemaining + " HR" }
+                        </span>
+                    );
+                } else {
+                    return (
+                        <span className="gb-hours-remaining">
+                            { hoursRemaining + " HRS" }
+                        </span>
+                    );
+                }
+            } else if(daysRemaining===1) {
+                return (
+                    <span className="gb-days-remaining">
+                        { daysRemaining + " DAY" }
+                    </span>
+                )
+            } else {
+                return (
+                    <span className="gb-days-remaining">
+                        { daysRemaining + " DAYS" }
+                    </span>
+                );
+            }
+        }
     }
 }
