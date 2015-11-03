264fbf70354a7eef80f7faa6f22f73d079a3afcb
Tenor Biel <tenorbiel@gmail.com>
2015-11-03 14:40:28 -0600

    Cleanup quantities and prices in menu items

--- app/components/restaurant/menu/item.js
+++ app/components/restaurant/menu/item.js
@@ -1,4 +1,5 @@
 import React, {Component, PropTypes} from 'react';
+import {FormattedNumber} from 'react-intl';
 
 import {MenuItem} from '../../../models/menu-item';
 
@@ -9,10 +10,15 @@ export class RestaurantMenuItemComponent extends Component {
 
     render() {
         const {item} = this.props;
-        const {name, tags, min_qty} = item.attributes;
+        const {
+            name,
+            tags,
+            min_qty,
+            price
+        } = item.attributes;
 
         return (
-            <div className="gb-restaurant-menu-item">
+            <div className="gb-restaurant-menu-item" onClick={this.handleClick}>
                 <div className="gb-restaurant-menu-item-title">
                     <div className="gb-restaurant-menu-item-title-content">
                         {name}
@@ -23,11 +29,17 @@ export class RestaurantMenuItemComponent extends Component {
                 </div>
 
                 <div className="gb-restaurant-menu-item-quantity">
-                    Min. 10
+                    {this.renderQuantity()}
                 </div>
 
                 <div className="gb-restaurant-menu-item-price">
-                    $20.00 per person
+                    <FormattedNumber
+                        value={price / 100}
+                        style="currency"
+                        currency="USD"
+                    />
+
+                    {min_qty ? ' per person' : null}
                 </div>
             </div>
         );
@@ -38,6 +50,19 @@ export class RestaurantMenuItemComponent extends Component {
                     {tagName}
                 </div>
             );
+        }
+    }
+
+    renderQuantity() {
+        const {item} = this.props;
+        const {min_qty, feeds_min, feeds_max} = item.attributes;
+
+        if (min_qty !== 0) {
+            return `Min. ${min_qty}`;
+        } else if (feeds_max > feeds_min) {
+            return `Feeds ${feeds_min}-${feeds_max}`;
+        } else {
+            return `Feeds ${feeds_min}`;
         }
     }
 }
