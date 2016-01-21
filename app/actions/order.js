bf8741f1c0ea830253d2be4e5599cd8b783dc46f
Tenor Biel <tenorbiel@gmail.com>
2016-01-21 12:23:29 -0600

    Add order info actions for order pane

--- app/actions/order.js
+++ app/actions/order.js
@@ -1,4 +1,4 @@
-export class UpdateOrderAction {
+export class UpdateOrderInfoAction {
     constructor({order, changes}) {
         this.order = order;
         this.changes = changes;
@@ -9,5 +9,11 @@ export class RemoveOrderItemAction {
     constructor({orderItem, order}) {
         this.orderItem = orderItem;
         this.order = order;
+    }
+}
+
+export class SubmitOrderInfoAction {
+    constructor({info}) {
+        this.info = info;
     }
 }
