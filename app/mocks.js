2ae55294283d13a2f198240770d5fb4458f56905
Tenor Biel <tenorbiel@gmail.com>
2016-01-27 12:11:26 -0600

    Add initial payload structures

--- app/mocks.js
+++ app/mocks.js
@@ -2,10 +2,12 @@ import {mocks as resolvers} from './resolvers/mocks';
 import {mocks as services} from './services/mocks';
 import {mocks as stores} from './stores/mocks';
 import {mocks as lib} from './lib/mocks';
+import {mocks as payloads} from './payloads/mocks';
 
 export const mocks = [
     ...resolvers,
     ...services,
     ...stores,
-    ...lib
+    ...lib,
+    ...payloads
 ];
