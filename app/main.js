50d0c85935aac64257f554e17a14d7506dfdb8ec
Tenor Biel <tenorbiel@gmail.com>
2016-02-16 15:34:24 -0600

    Handle unhandled rejections as top-level errors

--- app/main.js
+++ app/main.js
@@ -2,4 +2,9 @@ import {load} from './lib/load';
 
 const element = document.getElementById('gb-body');
 
+window.addEventListener('unhandledrejection', event => {
+    event.preventDefault();
+    throw event.detail.reason;
+});
+
 load(element);
