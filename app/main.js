42806df38634553408b30869ea562165c58cc71b
Tenor Biel <tenorbiel@gmail.com>
2016-04-25 11:31:40 -0500

    Alert on uncaught exceptions

--- app/main.js
+++ app/main.js
@@ -5,6 +5,9 @@ const element = document.getElementById('gb-body');
 
 window.addEventListener('unhandledrejection', event => {
     event.preventDefault();
+
+    alert('An expected error occured! Please refresh the page and trying again');
+
     throw event.detail.reason;
 });
 
